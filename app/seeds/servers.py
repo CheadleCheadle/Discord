c188e748f48afae5d53a5cc5b99225db870bdca0
Helen Koo <helenkoo2017@gmail.com>
2023-04-08 12:26:46 -0700

    update server seeder

--- app/seeds/servers.py
+++ app/seeds/servers.py
@@ -1,6 +1,6 @@
 from app.models import db, Server, environment, SCHEMA
 from sqlalchemy.sql import text
-import datetime
+from datetime import datetime
 
 
 
@@ -8,15 +8,21 @@ import datetime
 # Adds a demo server, you can add other servers here if you want
 def seed_servers():
     server1 = Server(
-        owner_id=1, icon_url='https://media.istockphoto.com/id/522001766/photo/woman-standing-near-big-tropical-river.jpg?s=612x612&w=0&k=20&c=hsUOZmY-CZtARYbk1wZuey7y1mfcUjfnj6PzXo7UgXA=',type=True, name="aA Open",max_users=30, description="Welcome to App Academy Nov 2022 Cohort",created_at=datetime.date.today())
+        owner_id=1, icon_url='https://upload.wikimedia.org/wikipedia/commons/7/7e/Appacademylogo.png',type=True, name="aA Open",max_users=30, description="Welcome to App Academy Nov 2022 Cohort",created_at=datetime.utcnow())
     server2 =Server(
-        owner_id=2, icon_url='https://media.discordapp.net/attachments/676180932809588767/1048993307528798288/bpwelcome22.jpg?width=1431&height=876',type=False, name="BLACK PINK",max_users=40, description="Welcome to BLACK PINK fans group",created_at=datetime.date.today())
+        owner_id=2, icon_url='https://media.discordapp.net/attachments/676180932809588767/1048993307528798288/bpwelcome22.jpg?width=1431&height=876',type=False, name="BLACK PINK",max_users=40, description="Welcome to BLACK PINK fans group",created_at=datetime.utcnow())
     server3 = Server(
-        owner_id=3, icon_url='https://www.shutterstock.com/shutterstock/photos/434536597/display_1500/stock-vector-vector-illustration-silhouette-of-a-climber-isolated-hiker-on-white-background-434536597.jpg',type=True, name="Hiker",max_users=50, description="Welcome to Moutain Hiker group",created_at=datetime.date.today())
+        owner_id=3, icon_url='https://www.shutterstock.com/shutterstock/photos/434536597/display_1500/stock-vector-vector-illustration-silhouette-of-a-climber-isolated-hiker-on-white-background-434536597.jpg',type=True, name="Hiker",max_users=50, description="Welcome to Moutain Hiker group",created_at=datetime.utcnow())
 
+    server4 = Server(
+        owner_id=1, icon_url='https://denverurban.net/wp-content/uploads/2014/05/gourmet-food.jpg',type=False, name="Recipes Sharing",max_users=35, description="Welcome to Recipes Sharing Group",created_at=datetime.utcnow())
+    server5 =Server(
+        owner_id=2, icon_url='https://www.thexboxhub.com/wp-content/uploads/2013/11/minecraftheader.jpg',type=True, name="Minecraft",max_users=45, description="Welcome to Minecraft group",created_at=datetime.utcnow())
+    
+    server6 = Server(
+        owner_id=3, icon_url='https://www.shutterstock.com/shutterstock/photos/434536597/display_1500/stock-vector-vector-illustration-silhouette-of-a-climber-isolated-hiker-on-white-background-434536597.jpg',type=False, name="Dog lover",max_users=55, description="Welcome to Dog lover group",created_at=datetime.utcnow())
 
-
-    all_servers = [server1,server2,server3]
+    all_servers = [server1,server2,server3,server4,server5,server6]
     add_servers = [db.session.add(server) for server in all_servers]
 
     
