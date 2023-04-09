072c067c3210b0b615fd5e1262db4fa7acfc258c
TKNY <zhtony0398@gmail.com>
2023-04-08 20:18:19 -0400

    latest

--- app/models/server.py
+++ app/models/server.py
@@ -1,37 +1,38 @@
 from app.models.db import db, environment, SCHEMA, add_prefix_for_prod
-import datetime
+from datetime import datetime
 
 # Join table for Servers and users to create memberships
-server_memberships = db.Table(
-    "server_memberships",  # Name of the table
-    db.Model.metadata,
-    db.Column("user_id", db.Integer, db.ForeignKey(
-        add_prefix_for_prod("users.id")), primary_key=True),
-    db.Column("server_id", db.Integer, db.ForeignKey(
-        add_prefix_for_prod("servers.id")), primary_key=True)
-)
+# server_memberships = db.Table(
+#     "server_memberships",  # Name of the table
+#     db.Model.metadata,
+#     db.Column("user_id", db.Integer, db.ForeignKey(
+#         add_prefix_for_prod("users.id")), primary_key=True),
+#     db.Column("server_id", db.Integer, db.ForeignKey(
+#         add_prefix_for_prod("servers.id")), primary_key=True)
+# )
 
 
-class Server(db.Modal):
+class Server(db.Model):
     __tablename__ = "servers"
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey(
-        add_prefix_for_prod("users.id"), nullable=False))
+    # owner_id = db.Column(db.Integer, db.ForeignKey(
+    #     add_prefix_for_prod("users.id"), nullable=False))
     icon_url = db.Column(db.String, nullable=True)
     public = db.Column(db.Boolean, nullable=False)
     name = db.Column(db.String(100), nullable=False, unique=True)
     max_users = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False,
-                           default=datetime.utcnow)
+    # created_at = db.Column(db.DateTime, nullable=False,
+    #                        default=datetime.utcnow)
 
-    # Relationship
-    users = db.relationship(
-        "User", secondary=server_memberships, back_populates="servers")
+    # # Relationship
+    # host = db.relationship("User", back_populates="servers")
+    # users = db.relationship(
+    #     "User", secondary=server_memberships, back_populates="servers")
 
     @property
     def icon_url(self):
@@ -42,12 +43,12 @@ class Server(db.Modal):
         self.icon_url = new_icon_url
 
     @property
-    def type(self):
-        return self.type
+    def public(self):
+        return self.public
 
-    @type.setter
-    def type(self, new_type):
-        self.type = new_type
+    @public.setter
+    def public(self, new_type):
+        self.public = new_type
 
     @property
     def name(self):
@@ -73,14 +74,14 @@ class Server(db.Modal):
     def description(self, new_description):
         self.description = new_description
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "owner_id": self.owner_id,
-            "icon_url": self.icon_url,
-            "type": self.type,
-            "name": self.name,
-            "max_users": self.max_users,
-            "description": self.description,
-            "created_at": self.created_at
-        }
+    # def to_dict(self):
+    #     return {
+    #         "id": self.id,
+    #         "owner_id": self.owner_id,
+    #         "icon_url": self.icon_url,
+    #         "type": self.type,
+    #         "name": self.name,
+    #         "max_users": self.max_users,
+    #         "description": self.description,
+    #         "created_at": self.created_at
+    #     }
--- app/seeds/servers.py
+++ app/seeds/servers.py
@@ -1,31 +1,28 @@
-from app.models import db, Server, environment, SCHEMA
+from app.models.db import db, Server, environment, SCHEMA
 from sqlalchemy.sql import text
 from datetime import datetime
-
-
 
 
 # Adds a demo server, you can add other servers here if you want
 def seed_servers():
     server1 = Server(
-        owner_id=1, icon_url='https://upload.wikimedia.org/wikipedia/commons/7/7e/Appacademylogo.png',type=True, name="aA Open",max_users=30, description="Welcome to App Academy Nov 2022 Cohort",created_at=datetime.utcnow())
-    server2 =Server(
-        owner_id=2, icon_url='https://media.discordapp.net/attachments/676180932809588767/1048993307528798288/bpwelcome22.jpg?width=1431&height=876',type=False, name="BLACK PINK",max_users=40, description="Welcome to BLACK PINK fans group",created_at=datetime.utcnow())
+        owner_id=1, icon_url='https://upload.wikimedia.org/wikipedia/commons/7/7e/Appacademylogo.png', type=True, name="aA Open", max_users=30, description="Welcome to App Academy Nov 2022 Cohort", created_at=datetime.utcnow())
+    server2 = Server(
+        owner_id=2, icon_url='https://media.discordapp.net/attachments/676180932809588767/1048993307528798288/bpwelcome22.jpg?width=1431&height=876', type=False, name="BLACK PINK", max_users=40, description="Welcome to BLACK PINK fans group", created_at=datetime.utcnow())
     server3 = Server(
-        owner_id=3, icon_url='https://www.shutterstock.com/shutterstock/photos/434536597/display_1500/stock-vector-vector-illustration-silhouette-of-a-climber-isolated-hiker-on-white-background-434536597.jpg',type=True, name="Hiker",max_users=50, description="Welcome to Moutain Hiker group",created_at=datetime.utcnow())
+        owner_id=3, icon_url='https://www.shutterstock.com/shutterstock/photos/434536597/display_1500/stock-vector-vector-illustration-silhouette-of-a-climber-isolated-hiker-on-white-background-434536597.jpg', type=True, name="Hiker", max_users=50, description="Welcome to Moutain Hiker group", created_at=datetime.utcnow())
 
     server4 = Server(
-        owner_id=1, icon_url='https://denverurban.net/wp-content/uploads/2014/05/gourmet-food.jpg',type=False, name="Recipes Sharing",max_users=35, description="Welcome to Recipes Sharing Group",created_at=datetime.utcnow())
-    server5 =Server(
-        owner_id=2, icon_url='https://www.thexboxhub.com/wp-content/uploads/2013/11/minecraftheader.jpg',type=True, name="Minecraft",max_users=45, description="Welcome to Minecraft group",created_at=datetime.utcnow())
-    
-    server6 = Server(
-        owner_id=3, icon_url='https://www.shutterstock.com/shutterstock/photos/434536597/display_1500/stock-vector-vector-illustration-silhouette-of-a-climber-isolated-hiker-on-white-background-434536597.jpg',type=False, name="Dog lover",max_users=55, description="Welcome to Dog lover group",created_at=datetime.utcnow())
+        owner_id=1, icon_url='https://denverurban.net/wp-content/uploads/2014/05/gourmet-food.jpg', type=False, name="Recipes Sharing", max_users=35, description="Welcome to Recipes Sharing Group", created_at=datetime.utcnow())
+    server5 = Server(
+        owner_id=2, icon_url='https://www.thexboxhub.com/wp-content/uploads/2013/11/minecraftheader.jpg', type=True, name="Minecraft", max_users=45, description="Welcome to Minecraft group", created_at=datetime.utcnow())
 
-    all_servers = [server1,server2,server3,server4,server5,server6]
+    server6 = Server(
+        owner_id=3, icon_url='https://www.shutterstock.com/shutterstock/photos/434536597/display_1500/stock-vector-vector-illustration-silhouette-of-a-climber-isolated-hiker-on-white-background-434536597.jpg', type=False, name="Dog lover", max_users=55, description="Welcome to Dog lover group", created_at=datetime.utcnow())
+
+    all_servers = [server1, server2, server3, server4, server5, server6]
     add_servers = [db.session.add(server) for server in all_servers]
 
-    
     db.session.commit()
     return add_servers
 
@@ -38,8 +35,9 @@ def seed_servers():
 # it will reset the primary keys for you as well.
 def undo_servers():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.servers RESTART IDENTITY CASCADE;")
+        db.session.execute(
+            f"TRUNCATE table {SCHEMA}.servers RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM servers"))
-        
+
     db.session.commit()
