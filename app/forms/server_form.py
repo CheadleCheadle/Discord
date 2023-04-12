dfe834589a67d29fa14539ed148ae8076860d34f
TKNY <zhtony0398@gmail.com>
2023-04-11 22:54:33 -0400

    latest

--- app/forms/server_form.py
+++ app/forms/server_form.py
@@ -5,9 +5,8 @@ from wtforms.validators import DataRequired
 
 class ServerForm(FlaskForm):
     icon_url = StringField('Image Url', validators=[DataRequired()])
-    public  = BooleanField('Public', validators=[DataRequired()])
+    public_ = BooleanField('Public', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
     max_users = IntegerField('Max Users', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('submit')
-
