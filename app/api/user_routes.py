7753a4c93cb36b452ab43420ffb85486f4165b4f
Grant <gcheadle123@gmail.com>
2023-04-13 08:54:18 -0700

    sockets are working

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -29,17 +29,21 @@ def user(id):
 @user_routes.route('/curr/messages/recipient/<int:recipient_id>', methods=["POST"])
 @login_required
 def get_messages_from_user(recipient_id):
-    # user_id = current_user.id
     data = json.loads(request.data)
     user_id = data["userId"]
-    print("user", user_id, "recep", recipient_id)
-    all_messages = DirectMessage.query.filter(
+    all_messages1 = DirectMessage.query.filter(
         DirectMessage.user_id == user_id,
         DirectMessage.recipient_id == recipient_id
     )
-    print('HERES ALL THE MESSAGES',[message.to_safe_dict() for message in all_messages])
+    all_messages_2 = DirectMessage.query.filter(
+        DirectMessage.recipient_id == user_id,
+        DirectMessage.user_id == recipient_id
+    )
+    all_messages = [x for n in (all_messages1, all_messages_2) for x in n]
+    filtered = [*set(all_messages)]
+    filtered.sort(key=lambda msg: msg._time_stamp)
+    return [message.to_safe_dict() for message in filtered]
 
-    return [message.to_safe_dict() for message in all_messages]
 # GET MESSAGES SENT FROM USER TO RECIPIENT
 
 
