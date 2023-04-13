15a17987ba0630c86d6d91562df9c40a380534ac
Grant <gcheadle123@gmail.com>
2023-04-13 13:03:25 -0700

    added all servers

--- react-app/src/components/ServerDetails/AllServersPage.js
+++ react-app/src/components/ServerDetails/AllServersPage.js
@@ -1,11 +1,20 @@
 import React from "react";
 import AllServersNavbar from "./AllServersNavbar";
+import { useSelector } from "react-redux";
 
 const AllServersPage = () => {
+    const servers = Object.values(useSelector(state => state.servers.allServers));
+    console.log("here are the servers",servers)
+    const handleClick = (server) => {
+    //open modal to join new server will be pending
+    }
   return (
     <>
       <AllServersNavbar />
-      <div>All servers</div>
+
+      {servers.map((server) => {
+        <div key={server.id}>{server.name}</div>
+      })}
     </>
   )
 }
