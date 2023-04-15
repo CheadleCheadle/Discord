42f49d58cee4fd206db689e158b5133c84e1d9aa
Helen Koo <helenkoo2017@gmail.com>
2023-04-14 19:50:00 -0700

    OpenModalButton

--- react-app/src/components/OpenModalButton/index.js
+++ react-app/src/components/OpenModalButton/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import { useModal } from '../../context/Modal';
+import React from "react";
+import { useModal } from "../../context/Modal";
 import "./modal.css";
 function OpenModalButton({
-  modalComponent, // component to render inside the modal
-  buttonText, // text of the button that opens the modal
-  modalCSSClass,
-  onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
-  onModalClose // optional: callback function that will be called once the modal is closed
+ modalComponent, // component to render inside the modal
+ buttonText, // text of the button that opens the modal
+ modalCSSClass,
+ onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
+ onModalClose, // optional: callback function that will be called once the modal is closed
 }) {
-  const { setModalContent, setOnModalClose } = useModal();
+ const { setModalContent, setOnModalClose } = useModal();
 
-  const onClick = () => {
-    if (onModalClose) setOnModalClose(onModalClose);
-    setModalContent(modalComponent);
-    if (onButtonClick) onButtonClick();
-  };
+ const onClick = () => {
+  if (onModalClose) setOnModalClose(onModalClose);
+  setModalContent(modalComponent);
+  if (onButtonClick) onButtonClick();
+ };
 
-  return (
-    <button className={modalCSSClass ? modalCSSClass : "modal-button"} onClick={onClick}>{buttonText}</button>
-  );
+ return (
+  <button
+   className={modalCSSClass ? modalCSSClass : "modal-button"}
+   onClick={onClick}
+  >
+   {buttonText}
+  </button>
+ );
 }
 
 export default OpenModalButton;
