90701994c950b9ed625e32e64b90869d690e3208
Helen Koo <helenkoo2017@gmail.com>
2023-04-15 04:37:02 -0700

    channel form

--- react-app/src/components/ServerDetails/SingleServerPage.js
+++ react-app/src/components/ServerDetails/SingleServerPage.js
@@ -8,57 +8,73 @@ import OpenModalMenuItem from "../OpenModalButton";
 import AddChannelModal from "../AddChannelModal";
 import ServerMenuBox from "../ServerMenuBox";
 import Friends from "../Friends";
-import "./Server.css"
+import "./Server.css";
 const SingleServerPage = () => {
-  const [ isLoaded, setIsLoaded ] = useState(false)
-  const dispatch = useDispatch()
-  const { serverId } = useParams();
-  const sessionUser = useSelector(state => state.session.user);
-  const servers = useSelector(state => state.servers.allServers);
-  console.log('servers-------------', servers);
-  useEffect(() => {
-    dispatch(getServerChannels(serverId))
-      .then(() => setIsLoaded(true))
-  }, [ dispatch, serverId ])
+ const [isLoaded, setIsLoaded] = useState(false);
+ const dispatch = useDispatch();
+ const { serverId } = useParams();
+ const sessionUser = useSelector((state) => state.session.user);
+ const servers = useSelector((state) => state.servers.allServers);
+ console.log("servers-------------", servers);
+ useEffect(() => {
+  dispatch(getServerChannels(serverId)).then(() => setIsLoaded(true));
+ }, [dispatch, serverId]);
 
-  const server = useSelector(state => state.servers.allServers[ serverId ])
-  // const channelsArr = Object.values(server.channels)
-  const channelsArr = Object.values(useSelector(state => state.channels.allChannels));
+ const server = useSelector((state) => state.servers.allServers[serverId]);
+ // const channelsArr = Object.values(server.channels)
+ const channelsArr = Object.values(
+  useSelector((state) => state.channels.allChannels)
+ );
 
-  return (
+ return (
+  <>
+   {isLoaded && (
     <>
-      {isLoaded && (
-        <>
-          <div className="svr-channel-wrapper">
-        <div className="svr-menu-box">
-            <ServerMenuBox servers={servers} user={sessionUser} />
-            <div className="chnl-container">
-
-              {channelsArr.map((channel) => (
-                <NavLink key={channel.id} to={`/servers/${serverId}/channels/${channel.id}`} >
-                  {channel.name} {channel.id}
-                </NavLink>
-              ))}
-            </div>
-          </div>
-              </div>
-          <Switch>
-            <Route path={`/servers/:serverId/channels/:channelId`}>
-              <Channel />
-            </Route>
-          </Switch>
-                {/* Button for opening the modal to create a channel. May need to be moved */}
-        {/* <OpenModalMenuItem
+     <div className="svr-channel-wrapper">
+      <div className="svr-menu-box">
+       <div className="svr-dropdown-btn-menu-box">
+        <ServerMenuBox servers={servers} user={sessionUser} />
+       </div>
+       <div className="chnl-container">
+        {channelsArr.map((channel) => (
+         <div className="chnl-container-item">
+          <NavLink
+           key={channel.id}
+           to={`/servers/${serverId}/channels/${channel.id}`}
+           style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "5px",
+            color: "white",
+           }}
+          >
+           {channel.name} {channel.id}
+          </NavLink>
+         </div>
+        ))}
+       </div>
+      </div>
+     </div>
+     <Switch>
+      <Route path={`/servers/:serverId/channels/:channelId`}>
+       <Channel />
+      </Route>
+     </Switch>
+     {/* Button for opening the modal to create a channel. May need to be moved */}
+     {/* <OpenModalMenuItem
         itemText="New Channel"
         modalComponent={<AddChannelModal />}
         /> */}
-          <div className="members-wrapper">
-          <Members></Members>
-          </div>
-        </>
-      )}
+     <div className="members-wrapper">
+      <Members></Members>
+     </div>
     </>
-  )
-}
+   )}
+  </>
+ );
+};
 
-export default SingleServerPage
+export default SingleServerPage;
--- react-app/src/store/servers.js
+++ react-app/src/store/servers.js
@@ -2,188 +2,198 @@ import { normalizeFn } from "./channels";
 import { newUserServer } from "./session";
 
 const LOAD_ALL_SERVERS = "servers/LOAD_ALL_SERVER";
+const LOAD_ALL_CURRENT_SERVERS = "servers/LOAD_ALL_CURRENT_SERVERS";
 const LOAD_ONE_SERVER = "servers/LOAD_ONE_SERVER";
 const ADD_A_SERVER = "servers/ADD_A_SERVER";
 const DELETE_A_SERVER = "servers/DELETE_A_SERVER";
 const EDIT_A_SERVER = "servers/EDIT_A_SERVER";
 const LOAD_ONE_SERVER_ID = "servers/ONE";
-export const loadOneServerId =(serverId) => {
-    return {
-      type: LOAD_ONE_SERVER_ID,
-      serverId
-    }
-}
+export const loadOneServerId = (serverId) => {
+ return {
+  type: LOAD_ONE_SERVER_ID,
+  serverId,
+ };
+};
 export const loadAllServers = (servers) => {
-  return {
-    type: LOAD_ALL_SERVERS,
-    servers,
-  };
+ return {
+  type: LOAD_ALL_SERVERS,
+  servers,
+ };
+};
+
+export const loadCurrentServers = (servers) => {
+ return {
+  type: LOAD_ALL_CURRENT_SERVERS,
+  servers,
+ };
 };
 
 export const loadOneServer = (server) => {
-  return {
-    type: LOAD_ONE_SERVER,
-    server,
-  };
+ return {
+  type: LOAD_ONE_SERVER,
+  server,
+ };
 };
 
 export const addAServer = (server) => {
-  return {
-    type: ADD_A_SERVER,
-    server,
-  };
+ return {
+  type: ADD_A_SERVER,
+  server,
+ };
 };
 export const deleteAServer = (id) => {
-  return {
-    type: DELETE_A_SERVER,
-    id,
-  };
+ return {
+  type: DELETE_A_SERVER,
+  id,
+ };
 };
 export const editAServer = (server) => {
-  return {
-    type: EDIT_A_SERVER,
-    server,
-  };
+ return {
+  type: EDIT_A_SERVER,
+  server,
+ };
 };
 export const thunkLoadAllServers = () => async (dispatch) => {
-  const response = await fetch("/api/servers/");
-  if (response.ok) {
-    const servers = await response.json();
-    const serversNormalized = normalizeFn(servers.servers)
-    for (let serverId in serversNormalized) {
-      const channels = serversNormalized[ serverId ].channels
-      serversNormalized[ serverId ].channels = normalizeFn(channels)
-      for (const channelId in serversNormalized[ serverId ].channels) {
-        const messages = serversNormalized[ serverId ].channels[ channelId ].channel_messages
-        serversNormalized[ serverId ].channels[ channelId ].channel_messages = normalizeFn(messages)
-      }
-    }
-    dispatch(loadAllServers(serversNormalized));
-    return serversNormalized
-  };
-  return
-}
+ const response = await fetch("/api/servers/");
+ if (response.ok) {
+  const servers = await response.json();
+  const serversNormalized = normalizeFn(servers.servers);
+  for (let serverId in serversNormalized) {
+   const channels = serversNormalized[serverId].channels;
+   serversNormalized[serverId].channels = normalizeFn(channels);
+   for (const channelId in serversNormalized[serverId].channels) {
+    const messages =
+     serversNormalized[serverId].channels[channelId].channel_messages;
+    serversNormalized[serverId].channels[channelId].channel_messages =
+     normalizeFn(messages);
+   }
+  }
+  dispatch(loadAllServers(serversNormalized));
+  return serversNormalized;
+ }
+ return;
+};
 
 export const thunkLoadOneServer = (id) => async (dispatch) => {
-  const response = await fetch(`/api/spots/${id}`);
-  const server = await response.json();
-  dispatch(loadOneServer(server));
+ const response = await fetch(`/api/spots/${id}`);
+ const server = await response.json();
+ dispatch(loadOneServer(server));
 };
 
 export const thunkAddAServer = (data) => async (dispatch) => {
-  console.log("reducer thunkAddAServer data:", data);
-  let server;
-  const response = await fetch("/api/servers/new", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
+ console.log("reducer thunkAddAServer data:", data);
+ let server;
+ const response = await fetch("/api/servers/new", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(data),
+ });
 
-  if (response.ok) {
-    server = await response.json();
-    dispatch(addAServer(server));
-    dispatch(newUserServer(server));
+ if (response.ok) {
+  server = await response.json();
+  dispatch(addAServer(server));
+  dispatch(newUserServer(server));
 
-    // console.log("reducer createASpot spot:", spot);
-    return server;
-  } else {
-    return response;
-  }
+  // console.log("reducer createASpot spot:", spot);
+  return server;
+ } else {
+  return response;
+ }
 };
 
 export const thunkDeleteAServer = (id) => async (dispatch) => {
-  //  console.log("reducer thunkAddAServer data:", data);
-  let message;
-  const response = await fetch(`/api/servers/${id}`, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
-    body: null,
-  });
+ //  console.log("reducer thunkAddAServer data:", data);
+ let message;
+ const response = await fetch(`/api/servers/${id}`, {
+  method: "DELETE",
+  headers: { "Content-Type": "application/json" },
+  body: null,
+ });
 
-  if (response.ok) {
-    message = await response.json();
-    dispatch(deleteAServer(+id));
+ if (response.ok) {
+  message = await response.json();
+  dispatch(deleteAServer(+id));
 
-    // console.log("reducer createASpot spot:", spot);
-    return message;
-  } else {
-    return response;
-  }
+  // console.log("reducer createASpot spot:", spot);
+  return message;
+ } else {
+  return response;
+ }
 };
 
 export const thunkEditAServer = (data, id) => async (dispatch) => {
-  //  console.log("reducer thunkAddAServer data:", data);
-  let server;
-  const response = await fetch(`/api/servers/${id}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
+ //  console.log("reducer thunkAddAServer data:", data);
+ let server;
+ const response = await fetch(`/api/servers/${id}`, {
+  method: "PUT",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(data),
+ });
 
-  if (response.ok) {
-    server = await response.json();
-    dispatch(editAServer(server));
+ if (response.ok) {
+  server = await response.json();
+  dispatch(editAServer(server));
 
-    // console.log("reducer createASpot spot:", spot);
-    return server;
-  } else {
-    return response;
-  }
+  // console.log("reducer createASpot spot:", spot);
+  return server;
+ } else {
+  return response;
+ }
 };
 
-const initialState = { allServers: {}, singleServerId: null};
+const initialState = { allServers: {}, singleServerId: null };
 
 const serverReducer = (state = initialState, action) => {
-  //console.log("Inside serverReducer: ", action.type);
-  let newState = {};
-  switch (action.type) {
-    case LOAD_ONE_SERVER_ID: {
-      newState = {
-        ...state,
-        singleServerId: action.serverId
-      }
-      return newState;
-    }
-    case LOAD_ALL_SERVERS:
-      newState = {
-        ...state,
-        allServers: { ...action.servers },
-      };
-      return newState;
-    case LOAD_ONE_SERVER:
-      newState = {
-        ...state,
-        allServers: {},
-      };
-      newState.allServers[ action.server.id ] = action.server;
-      return newState;
-
-    case ADD_A_SERVER:
-      newState = {
-        ...state,
-        allServers: { ...state.allServers },
-      };
-      newState.allServers[ action.server.id ] = action.server;
-      return newState;
-
-    case DELETE_A_SERVER:
-      newState = {
-        ...state,
-        allServers: { ...state.allServers },
-      };
-      delete newState.allServers[ action.id ];
-      return newState;
-
-    case EDIT_A_SERVER:
-      newState = {
-        ...state,
-        allServers: { ...state.allServers },
-      };
-      newState.allServers[ action.server.id ] = action.server;
-      return newState;
-
-    default:
-      return state;
+ //console.log("Inside serverReducer: ", action.type);
+ let newState = {};
+ switch (action.type) {
+  case LOAD_ONE_SERVER_ID: {
+   newState = {
+    ...state,
+    singleServerId: action.serverId,
+   };
+   return newState;
   }
+  case LOAD_ALL_SERVERS:
+   newState = {
+    ...state,
+    allServers: { ...action.servers },
+   };
+   return newState;
+  case LOAD_ONE_SERVER:
+   newState = {
+    ...state,
+    allServers: {},
+   };
+   newState.allServers[action.server.id] = action.server;
+   return newState;
+
+  case ADD_A_SERVER:
+   newState = {
+    ...state,
+    allServers: { ...state.allServers },
+   };
+   newState.allServers[action.server.id] = action.server;
+   return newState;
+
+  case DELETE_A_SERVER:
+   newState = {
+    ...state,
+    allServers: { ...state.allServers },
+   };
+   delete newState.allServers[action.id];
+   return newState;
+
+  case EDIT_A_SERVER:
+   newState = {
+    ...state,
+    allServers: { ...state.allServers },
+   };
+   newState.allServers[action.server.id] = action.server;
+   return newState;
+
+  default:
+   return state;
+ }
 };
 export default serverReducer;
