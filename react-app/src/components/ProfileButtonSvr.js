6115dd0fa0f069eb2c7a2dc4748f6c909a7a00a2
Helen Koo <helenkoo2017@gmail.com>
2023-05-19 03:01:20 -0700

    fix nav bar, new server form and new channel form

---
+++ react-app/src/components/ProfileButtonSvr.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../store/session";
+import OpenModalButton from "../components/OpenModalButton";
+import LoginFormModal from "../components/LoginFormModal";
+import SignupFormModal from "../components/SignupFormModal";
+import { useHistory } from "react-router-dom";
+
+function ProfileButtonSvr({ user }) {
+ const history = useHistory();
+ const dispatch = useDispatch();
+ const [showMenu, setShowMenu] = useState(false);
+ const ulRef = useRef();
+
+ const openMenu = () => {
+  if (showMenu) return;
+  setShowMenu(true);
+ };
+
+ useEffect(() => {
+  if (!showMenu) return;
+
+  const closeMenu = (e) => {
+   if (!ulRef.current.contains(e.target)) {
+    setShowMenu(false);
+   }
+  };
+
+  document.addEventListener("click", closeMenu);
+
+  return () => document.removeEventListener("click", closeMenu);
+ }, [showMenu]);
+
+ const handleLogout = (e) => {
+  e.preventDefault();
+  dispatch(logout()).then(() => history.push("/"));
+ };
+
+ const closeMenu = () => setShowMenu(false);
+ const ulClassName = "svr-profile-dropdown" + (showMenu ? "" : " hidden");
+
+ return (
+  <>
+   <div className="svr-dropdown">
+    <button onClick={openMenu}>
+     <i className="fas fa-user-circle fa-3x svr-profile-btn-color" />
+    </button>
+    <div className={ulClassName} ref={ulRef}>
+     <div className="dropdown-content">{"Hello, " + user.firstName}</div>
+     <div className="dropdown-content">{user.email}</div>
+     <div className="dropdown-content logout-button-container">
+      <button className="logout-button" onClick={handleLogout}>
+       Log Out
+      </button>
+     </div>
+    </div>
+   </div>
+  </>
+ );
+}
+
+export default ProfileButtonSvr;
--- react-app/src/components/ServerDetails/AllServersNavbar.js
+++ react-app/src/components/ServerDetails/AllServersNavbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import ProfileButton from "../Navigation/ProfileButton";
+import ProfileButtonSvr from "../ProfileButtonSvr";
 import NavBarServerList from "../NavBarServerList";
 import OpenModalButton from "../OpenModalButton";
 import AddServerForm from "../AddServerForm";
@@ -10,46 +10,46 @@ import OpenModalMenuItem from "../OpenModalButton";
 import AddChannelModal from "../AddChannelModal";
 
 const AllServersNavbar = () => {
-  const return_servers = useSelector((state) => state.servers.allServers);
-  const sessionUser = useSelector((state) => state.session.user);
-  // const servers = Object.values(return_servers);
-  //  const server
-  const history = useHistory();
-  const goHome = () => {
-    return history.push("/servers");
-  };
-  return (
-    <>
-      <div className="svr-nav-bar">
-        <div className="svr-nav-server-list">
-          <div id="home" onClick={() => goHome()}>
-            <img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png"></img>
-          </div>
-          <NavBarServerList />
-        </div>
-        <div className="svr-nav-menu">
-          {/* <div className="svr-nav-menu-item svr-dropdown-parent"> */}
-          <ProfileButton user={sessionUser} />
-          {/* </div> */}
-          <div className="svr-nav-menu-item">
-            <OpenModalButton
-              modalCSSClass="svr-new-server-button"
-              buttonText="➕"
-              modalComponent={<AddServerForm user={sessionUser} />}
-            />
-          </div>
-          <div className="svr-nav-menu-item">
-            <OpenModalMenuItem
-              modalCSSClass="svr-new-channel-button"
-              buttonText="➕"
-              itemText="New Channel"
-              modalComponent={<AddChannelModal />}
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+ const return_servers = useSelector((state) => state.servers.allServers);
+ const sessionUser = useSelector((state) => state.session.user);
+ // const servers = Object.values(return_servers);
+ //  const server
+ const history = useHistory();
+ const goHome = () => {
+  return history.push("/servers");
+ };
+ return (
+  <>
+   <div className="svr-nav-bar">
+    <div className="svr-nav-server-list">
+     <div id="home" onClick={() => goHome()}>
+      <img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png"></img>
+     </div>
+     <NavBarServerList />
+    </div>
+    <div className="svr-nav-menu">
+     {/* <div className="svr-nav-menu-item svr-dropdown-parent"> */}
+     <ProfileButtonSvr user={sessionUser} />
+     {/* </div> */}
+     <div className="svr-nav-menu-item">
+      <OpenModalButton
+       modalCSSClass="svr-new-server-button"
+       buttonText="➕"
+       modalComponent={<AddServerForm user={sessionUser} />}
+      />
+     </div>
+     <div className="svr-nav-menu-item">
+      <OpenModalMenuItem
+       modalCSSClass="svr-new-channel-button"
+       buttonText="➕"
+       itemText="New Channel"
+       modalComponent={<AddChannelModal />}
+      />
+     </div>
+    </div>
+   </div>
+  </>
+ );
 };
 
 export default AllServersNavbar;
