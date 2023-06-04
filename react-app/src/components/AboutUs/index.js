7dedaaa27db929e37d07c4bd7bcce6b1a3855c23
Grant <gcheadle123@gmail.com>
2023-06-03 17:29:09 -0700

    added about us

--- react-app/src/components/AboutUs/index.js
+++ react-app/src/components/AboutUs/index.js
@@ -1,10 +1,18 @@
-
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 import "./main.css";
 export default function AboutUs() {
     return (
+        <div id="about-us-container">
+        <Link id="back" to="/servers">
+            <FontAwesomeIcon icon={faArrowLeftLong} />
+            Back
+            </Link>
+
         <div className="about-us-cont">
         <div id="about-me-cont">
-
+            <h1>Grant A. Cheadle</h1>
             <div className="about-info">
                 <div className="grant pfp"></div>
             </div>
@@ -15,34 +23,7 @@ export default function AboutUs() {
 
 
             <div className="about-info">
-                <h3>Details</h3>
-                <span>
-                    <p>Technologies Used</p>
-                    <ul id="technologies">
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/javascript-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/react-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/redux-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://www.vectorlogo.zone/logos/sequelizejs/sequelizejs-icon.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://img.icons8.com/ios/50/express-js.png" alt="express-js" />
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/node-js-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/css-3-svgrepo-com.svg"></img>
-                        </li>
-
-                    </ul>
-                </span>
+                <h3 id="details">Details</h3>
                 <span>
                     <div id="externals">
 
@@ -50,10 +31,11 @@ export default function AboutUs() {
                             <i class="fa-brands fa-linkedin"></i>
                         </a>
 
+                        <a id="portfolio" href="https://cheadlecheadle.github.io/" target="_blank">Portfolio</a>
+
                         <a id="linkedin" href="https://github.com/CheadleCheadle" target="_blank">
                             <i class="fa-brands fa-github"></i>
                         </a>
-                        <a id="portfolio" href="https://cheadlecheadle.github.io/" target="_blank">Portfolio</a>
                     </div>
                 </span>
 
@@ -62,55 +44,30 @@ export default function AboutUs() {
         </div>
         <div id="about-me-cont">
 
+            <h1>Chunyi (Helen) Koo</h1>
             <div className="about-info">
                 <div className="helen pfp"></div>
             </div>
             <div className="about-info">
                 <h3>About me</h3>
-                <p>I specialize in front-end technologies, including React.js and Vue.js, and excel in programming languages such as Javascript, Python, and SQL. Currently, I am honing my expertise in React.js while delving into innovative languages like Rust and TypeScript to develop robust, scalable, and secure applications. Driven by a passion for delivering results, I am well-equipped to confront any challenge and make a significant impact within any team!</p>
+                <p>I'm Helen, a software developer with a degree in computer science from 2001. Since then, I've worked in various roles, including product engineering and NPI project manager at Foxconn, an international manufacturing company. Through my previous jobs, I have developed problem-solving skills, attention to detail, and the ability to work in a team.</p>
             </div>
 
 
             <div className="about-info">
-                <h3>Details</h3>
-                <span>
-                    <p>Technologies Used</p>
-                    <ul id="technologies">
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/javascript-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/react-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/redux-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://www.vectorlogo.zone/logos/sequelizejs/sequelizejs-icon.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://img.icons8.com/ios/50/express-js.png" alt="express-js" />
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/node-js-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/css-3-svgrepo-com.svg"></img>
-                        </li>
-
-                    </ul>
-                </span>
+                <h3 id="details">Details</h3>
                 <span>
                     <div id="externals">
 
-                        <a id="linkedin" href="https://www.linkedin.com/in/grant-a-cheadle-0233771a7/" target="_blank">
+                        <a id="linkedin" href="https://www.linkedin.com/in/chunyi-koo-70780025a/" target="_blank">
                             <i class="fa-brands fa-linkedin"></i>
                         </a>
 
-                        <a id="linkedin" href="https://github.com/CheadleCheadle" target="_blank">
+                        <a id="portfolio" href="https://chunyikoo.github.io/" target="_blank">Portfolio</a>
+
+                        <a id="linkedin" href="https://github.com/ChunyiKoo" target="_blank">
                             <i class="fa-brands fa-github"></i>
                         </a>
-                        <a id="portfolio" href="https://cheadlecheadle.github.io/" target="_blank">Portfolio</a>
                     </div>
                 </span>
 
@@ -120,60 +77,38 @@ export default function AboutUs() {
 
         <div id="about-me-cont">
 
+            <h1>Tony Zheng</h1>
             <div className="about-info">
                 <div className="tony pfp"></div>
             </div>
             <div className="about-info">
                 <h3>About me</h3>
-                <p>I specialize in front-end technologies, including React.js and Vue.js, and excel in programming languages such as Javascript, Python, and SQL. Currently, I am honing my expertise in React.js while delving into innovative languages like Rust and TypeScript to develop robust, scalable, and secure applications. Driven by a passion for delivering results, I am well-equipped to confront any challenge and make a significant impact within any team!</p>
+                <p>Goal-driven and adaptable professional with a passion for taking on challenges and delivering results. With an extensive background in Marketing and Communications, I bring unique perspectives and valuable experiences to any team.
+
+Skilled in interpersonal communication and collaboration, I excel at connecting with people from various backgrounds and ideologies, fostering an inclusive and supportive environment for success.</p>
             </div>
 
 
             <div className="about-info">
-                <h3>Details</h3>
-                <span>
-                    <p>Technologies Used</p>
-                    <ul id="technologies">
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/javascript-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/react-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/redux-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://www.vectorlogo.zone/logos/sequelizejs/sequelizejs-icon.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://img.icons8.com/ios/50/express-js.png" alt="express-js" />
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/node-js-svgrepo-com.svg"></img>
-                        </li>
-                        <li>
-                            <img src="https://cheadlecheadle.github.io/css-3-svgrepo-com.svg"></img>
-                        </li>
-
-                    </ul>
-                </span>
+                <h3 id="details">Details</h3>
                 <span>
                     <div id="externals">
 
-                        <a id="linkedin" href="https://www.linkedin.com/in/grant-a-cheadle-0233771a7/" target="_blank">
+                        <a id="linkedin" href="https://www.linkedin.com/in/tony-zheng-577840156/" target="_blank">
                             <i class="fa-brands fa-linkedin"></i>
                         </a>
 
-                        <a id="linkedin" href="https://github.com/CheadleCheadle" target="_blank">
+                        <a id="portfolio" href="https://usr1l.github.io/" target="_blank">Portfolio</a>
+                        <a id="linkedin" href="https://github.com/usr1l" target="_blank">
+
                             <i class="fa-brands fa-github"></i>
                         </a>
-                        <a id="portfolio" href="https://cheadlecheadle.github.io/" target="_blank">Portfolio</a>
                     </div>
                 </span>
 
             </div>
 
+        </div>
         </div>
         </div>
     )
